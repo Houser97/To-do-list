b3b5bb914285b09a0e5a29c6dc73b150a1fbad06
Arturo Rivera <arturo.riverar97@gmail.com>
2022-05-16 09:44:29 -0500

    Add new list elements with JavaScript
    This was done by creating a DOM object with the method addNewListElement
    which oversees when the submit button of the corresponding form is
    pressed, it also checks that the input has some value, otherwise it
    returns nothing.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './style.css'
 
+const DOM = ((doc) => {
+
+    function addElementList(e) {
+        e.preventDefault();
+        const listElement = doc.querySelector('[data-lists]');
+        let li = doc.createElement('li');
+            li.classList.add('list-option');
+        let input = doc.querySelector('[data-new-list-input]');
+
+        if(input.value == null || input.value === '') return;
+
+            li.innerText = input.value;
+            listElement.appendChild(li);
+            input.value = '';
+    }
+
+    const addNewListElement = () => {
+        const newListForm = doc.querySelector('[data-new-list-form]');
+        newListForm.addEventListener('submit', addElementList)
+    }
+
+    return {addNewListElement}
+})(document);
+
+DOM.addNewListElement();
+
+const navbar = ((doc)=>{
+    const addNewList = () => {
+        const buttonAddList = doc.querySelector('.createListBtn');
+        buttonAddList.addEventListener('click', )
+    }
+})(document);
+
 console.log('Houmser Weno');
